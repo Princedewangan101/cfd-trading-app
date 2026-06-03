7ea5825ad58878390dd33a663d4a781cd4f69bc5
Princedewangan101 <dewanganprince5151@gmail.com>
2026-06-03 12:31:16 +0530

    add:save candle to db

--- poller/src/main/server.ts
+++ poller/src/main/server.ts
@@ -1,17 +1,18 @@
 import "dotenv/config";
 import express, {type Request, type Response} from "express";
+import { startPoller } from "./poller";
 
 
 const app = express();
 // app.use(cors())
 app.use(express.json())
-// startPoller();
+startPoller();
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from the Express TypeScript server!", path: req.path });
 });
 
-const port = 5000;
+const port = 5002;
 app.listen(port, () => {
   console.log(`server running at ${port}`);
 });
