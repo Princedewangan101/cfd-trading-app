588ad74d98232a9c4b7ad9175e549e462d1d252d
Princedewangan101 <dewanganprince5151@gmail.com>
2026-07-01 00:58:54 +0530

    modified: bal mang in deposit route

--- backend/src/main/handlers/deposit.ts
+++ backend/src/main/handlers/deposit.ts
@@ -5,27 +5,47 @@ import { setIdemResponse } from '../util/IdempotencyResponseUpdate.js';
 import { TransactionType } from '../../type/type.js';
 import { check } from '../util/IdempotencyCheck.js';
 
-
+// - FETCH LOCKED BAL , IF WE DO IT AFTER TRAN. AND THE FETCH REQ FAILS THEN WE HAVE TO REVERT THE TRAN.
+// - ADDING DEPOSIT IN TRAN RECORD & INCR AVA. BAL. IN USER ROW.
+// - UPDATING TOTAL BAL. IN REDIS FOR USER
 
 export async function deposit(req: Request, res: Response) {
-    // console.log("\n\n>> /api/deposit");
+    console.log("\n\n>> /api/deposit");
 
     const userId = req.userId;
     const { ikey, amount } = req.body;
-    
-    if (!ikey || !userId || !amount) { return res.status(404).json({ success: false, message: "Missing required fields !" }) }
-    
-    // console.log(`> ikey: ${ikey}, amount:${amount}`);
-    
+
+    if (!ikey || !userId || !amount) {
+        console.log("\n> ------- ERROR : Missing required fields !");
+        return res.status(404).json({ success: false, message: "Missing required fields !" })
+    }
+
     try {
         await check(res, ikey, userId, "deposit")
-        
-        // console.log(1);
-        
+
+        let lockedBalance;
+        lockedBalance = await redis.get(`lockedBalance:${userId}`);
+        console.log(`\n> lockedBalance (GET FROM CACHE) : ${lockedBalance}`);
+
+        if (!lockedBalance) {
+            const lockedBalanceQuery = await prisma.user.findUnique({
+                where: {
+                    userId: String(userId), select: { lockedBalance: true }
+                }
+            })
+            if (!lockedBalanceQuery) {
+                console.log("\n> ------- ERROR : Failed to get locked balance. !");
+                return res.status(404).json({ success: false, message: "Failed to get locked balance." })
+            }
+            lockedBalance = lockedBalanceQuery.lockedBalance
+            await redis.set(`lockedBalance:${userId}`, String(lockedBalance), "EX", 3600)
+            console.log(`\n> lockedBalance (FETCH FROM DB) : ${lockedBalance}`);
+        }
+
         const result = await prisma.$transaction(async (tx: any) => {
             await tx.$queryRaw`SELECT * FROM "User" WHERE "userId" = ${userId} FOR UPDATE`;
             const updateBalanceResult = await tx.user.update({
-                where: { userId: userId },
+                where: { userId: String(userId) },
                 data: { availableBalance: { increment: Number(amount) } }
             });
             const transactionResult = await tx.transaction.create({
@@ -42,20 +62,21 @@ export async function deposit(req: Request, res: Response) {
             return res.status(400).json({ success: false, message: "Failed to deposit." })
         }
 
-        // console.log(2);
-
         await redis.set(`availableBalance:${userId}`, String(result.availableBalance), "EX", 3600);
 
+        const totalBalance = String(Number(result.availableBalance) + Number(lockedBalance))
+        await redis.set(`totalBalance:${userId}`, totalBalance, "EX", 3600)
+
         await setIdemResponse(ikey, userId, result.transactionId);
-        // console.log("------------------completed");
+        console.log("------------------ completed");
 
         return res.status(200).json({ success: true, transactionId: result.transactionId, message: "Deposit successful." })
 
     } catch (error: any) {
         console.log("\nERROR (deposit) : ", error.message);
-        await redis.set(`deposit${ikey}`, `${error.message}`);
+        await redis.set(`deposit${ikey}`, `${error.message}`, "EX", 3600);
         await setIdemResponse(ikey, userId, `${error.message}`);
-        // console.log("------------------error");
+        console.log("------------------ error");
         return res.status(500).json({ success: false, message: `Server error !` });
     }
 }
